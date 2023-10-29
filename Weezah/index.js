d384eed5ac81d76561c68dbe34893573219a8ce0
Malcolm Chris-Ogar <55517766+Spliff525@users.noreply.github.com>
2023-10-29 03:10:38 +0100

    Add files via upload

---
+++ Weezah/index.js
@@ -0,0 +1,90 @@
+//sidebar
+
+
+
+
+
+
+
+const menuItems = document.querySelectorAll('.menu-item');
+//remove active class from all menu items
+
+
+//messages
+const messagesNotification = document.querySelector('#messages-notification');
+const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
+
+ 
+const changeActiveItem = () => {
+    menuItems.forEach(item => {
+        item.classList.remove('active');
+    })
+}
+
+
+
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        changeActiveItem();
+        item.classList.add('active');
+
+if(item.id !='notifications'){
+    document.querySelector('.notifications-popup').style.display='none';
+}
+else{
+    document.querySelector('.notifications-popup').style.display='block';
+document.querySelector('#notifications .notification-count').style.display='none';
+
+}
+
+
+      })
+})
+
+
+//  Search chat
+
+
+const searchMessage =()=>{
+    const val = messageSearch.value.toLowerCase();
+    
+    message.forEach(chat =>
+        { let name= chat.querySelectorAll('h5').textContent.toLowerCase();
+    if(name.indexOf(val)!=-1) {
+        chat.style.display='flex';
+    }
+    else{
+        chat.style.display='none'
+    }
+
+    })
+}
+
+
+
+//search chat
+messageSearch.addEventListener('keyup', searchMessage);
+
+
+
+
+
+
+
+/*messages*/
+messagesNotification.addEventListener('click', ()=>{
+messages.style.boxShadow ='0 0 1rem var(--color-primary)';
+messagesNotification.querySelector('.notification-count').style.display='none';
+
+setTimeout(()=> {
+messages.style.boxShadow='none';
+
+
+}, 2000);
+
+
+} )
+
